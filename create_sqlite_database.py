3184c1395351c5f87a4e2e1612c893795368b96c
dnlrkorn@gmail.com <dnlrkorn@gmail.com>
2020-06-03 22:34:08 +0000

    Clean up part 2

--- create_sqlite_database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('coached_validate.db')
-c = conn.cursor()
-
-c.execute('''CREATE TABLE targets
-                 (idx text, entry_name text, status text, protein_names text, gene_names text, organism text, length int)''')
-
-
